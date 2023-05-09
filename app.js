c534c910eee7733690e517fcae0f33413357f58d
RBlytics <agus.rodbel@gmail.com>
2023-05-09 09:52:24 -0300

    ultimas modificaciones

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-const port = 3000;
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`) );
-
 const home = (req, res) => res.sendFile(path.join(__dirname, './views/home.html'));
 const register = (req, res) => res.sendFile(path.join(__dirname, './views/register.html'));
 const login = (req, res) => res.sendFile(path.join(__dirname, './views/login.html'));
@@ -15,3 +12,10 @@ const login = (req, res) => res.sendFile(path.join(__dirname, './views/login.htm
 app.get('/', home)
 app.get('/register', register)
 app.get('/login', login)
+
+const port = process.env.PORT || 3001;
+app.listen(port, ()=> console.log(`Servidor corriendo en el puerto ${port}`));
+
+/* const port = 3000;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`)); */
+
